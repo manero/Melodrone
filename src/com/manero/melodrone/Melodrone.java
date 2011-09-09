a01769e413ef3fe5343b5f5764b644df40acf8a7
manero <cassianofcr@gmail.com>
2011-09-09 01:43:17 -0300

    things changed a bit; our model is now an enum, our update cycles through the screen and changes our notes accordingly. TODO: invalidate the screen, play the sound

--- src/com/manero/melodrone/Melodrone.java
+++ src/com/manero/melodrone/Melodrone.java
@@ -1,33 +1,41 @@
 package com.manero.melodrone;
 
+import java.util.Timer;
+
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.Log;
 
 
 public class Melodrone {
+	public enum NoteState {
+		OFF,
+		ON,
+		PLAYING,
+	}
 	
 	static int GRID_SIDE = 16;
 	//model
-	int mBeat = 0; // 0 to 15
-	boolean[][] mNotes = 	{
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+	int mCurrentBeat = 0; // 0 to 15
+	NoteState[][] mNotes = 	{
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
+			{NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF, NoteState.OFF},
 							};
 	
 	
@@ -43,12 +51,66 @@ public class Melodrone {
 		mWidth = width;
 		mHeight = height;
 		mCellSize = new Rect(0,0, width/GRID_SIDE, height/GRID_SIDE);
+		Thread thread = new Thread() {
+			@Override
+            public void run() {
+				while(true) {
+	                Melodrone.this.update();
+	                try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+            }
+        };
+		thread.start();
 	}
 
 	public void update() {
-		mBeat++;
-		if (mBeat >= 16)
-			mBeat = 0;
+		if (mCurrentBeat >= GRID_SIDE)
+			mCurrentBeat = 0;
+		for (int i = 0; i < GRID_SIDE; i++) {
+//			int lastBeat = (mCurrentBeat - 1) % 16;
+			int lastBeat = mCurrentBeat - 1;
+			if (lastBeat == -1) lastBeat = 15;
+			if (mNotes[mCurrentBeat][i] == NoteState.ON) {
+				mNotes[mCurrentBeat][i] = NoteState.PLAYING;
+			}
+			if (mNotes[lastBeat][i] == NoteState.PLAYING) {
+				mNotes[lastBeat][i] = NoteState.ON;
+			}
+		}
+		mCurrentBeat++;
+		printGrid();
+	}
+
+	private void printGrid() {
+		Log.i("grid", "mCurrentBeat = " + mCurrentBeat);
+		Log.i("grid", "  -------------------------");
+		for (int i = 0; i < GRID_SIDE; i++) {
+			String currentLine = " |";
+			for (int j = 0; j < GRID_SIDE; j++) {
+				switch (mNotes[j][i]) {
+				case OFF:
+					currentLine = currentLine.concat(" ");
+					break;
+				case ON:
+					currentLine = currentLine.concat("o");
+					break;
+				case PLAYING:
+					currentLine = currentLine.concat("X");
+					break;
+
+				default:
+					break;
+				}
+				
+			}
+			currentLine = currentLine.concat("|");
+			Log.i("grid", currentLine);
+		}
+		Log.i("grid", "  -------------------------");
 	}
 	
 	public void draw(Canvas canvas, Paint paint){
@@ -58,10 +120,19 @@ public class Melodrone {
 				drawRect.set(mCellSize);
 				drawRect.offset(i*mCellSize.width(), j*mCellSize.height());
 				
-				if (mNotes[i][j] == true) {
-					paint.setColor(Color.WHITE);
-				} else {
+				switch (mNotes[i][j]) {
+				case OFF:
+					paint.setColor(Color.BLACK);
+					break;
+				case ON:
 					paint.setColor(Color.GRAY);
+					break;
+				case PLAYING:
+					paint.setColor(Color.WHITE);
+					break;
+
+				default:
+					break;
 				}
 				canvas.drawRect(drawRect, paint);
 			}
@@ -73,17 +144,10 @@ public class Melodrone {
 		int column = (int) (x / mCellSize.width());
 		int row = (int) (y / mCellSize.height());
 		
-//		if (mNotes[column][row])
-//			lightingUp = false;
-//		else
-//			lightingUp = true;
-//		
-//		if (lightingUp)
-//			mNotes[column][row] = true;
-//		else
-//			mNotes[column][row] = false;
-		mNotes[column][row] = !mNotes[column][row]; 
-//		return mNotes[column][row];
+		if (mNotes[column][row] == NoteState.OFF)
+			mNotes[column][row] = NoteState.ON;
+		else
+			mNotes[column][row] = NoteState.OFF;
 		return true;
 	}
 }
